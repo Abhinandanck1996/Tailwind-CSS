046ace7a3015b53e78c4ffa3c8b71e152bb7646f
Abhinandan CK <abhinandanck1996@gmail.com>
2022-03-26 16:38:46 +0530

    tailwind css

---
+++ plugin.js
@@ -0,0 +1,20 @@
+const plugin = require('tailwindcss/plugin');
+const bsComponents = require('./bs-styles');
+
+module.exports = plugin(
+  ({ addComponents }) => {
+    addComponents(bsComponents);
+  },
+  {
+    theme: {
+      extend: {
+        
+        fontFamily: {
+          sans: ['Inter', 'sans-serif'],
+          body: ['Inter', 'sans-serif'],
+          mono: ['ui-monospace', 'monospace'],
+        },
+      },
+    },
+  }
+);
